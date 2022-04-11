11422257f151f47f3b417d19d8684406f2f34e60
akarki7 <karkiaabishkar@gmail.com>
2022-04-11 11:42:56 +0200

    Added gitignore and base of main file

--- code/main.c
+++ code/main.c
@@ -1,25 +1,91 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include <time.h>
+#include<time.h>
+#include<stdbool.h>
+#include<errno.h>
+#include <unistd.h>
+
+void execute_bschedule();
 
 int main(){
     float frequency;
-    int i;
+    float input_in_seconds;
+    int i,choice;
     long double time_in_seconds;
+    long double time_in_nanoseconds;
+    bool nsecs=true;
 
-    printf("Enter frequency in Hz = ");
-    scanf("%f", &frequency);
+    struct timespec tim;
+    tim.tv_sec=0;
 
-    time_in_seconds= (long double)1/(long double)frequency;
+    printf("Enter your choice for input:\n1-Input in Seconds\n2-Input in Hz\n\n");
+    scanf("%d", &choice);
 
-    printf("\nTime in seconds = %.20Lf\n",time_in_seconds); 
-
-    for (i=0;i<2;i++)
-    {
-        system("./out");
-        // _execl();
-        printf("Finished\n ");
+    switch (choice){
+        case 1:
+            printf("Enter time in seconds (>=1) = ");
+            scanf("%f", &input_in_seconds);
+            if (input_in_seconds <1){
+                printf("Invalid input.Exiting....\n");
+                exit(EXIT_FAILURE);
+            }
+            nsecs=false;
+            break;
+        case 2:
+            printf("Enter frequency in Hz (>=1) = ");
+            scanf("%f", &frequency);
+            if (frequency <1){
+                printf("Invalid input.Exiting....\n");
+                exit(EXIT_FAILURE);
+            }
+            time_in_seconds= (long double)1/(long double)frequency;
+            time_in_nanoseconds=time_in_seconds*1000000000;
+            tim.tv_nsec=time_in_nanoseconds;
+            printf("\nTime in seconds = %.20Lf\n",time_in_seconds); 
+            printf("\nTime in nanoseconds = %.20Lf\n",time_in_nanoseconds); 
+            if(time_in_nanoseconds>= 1000000000)
+            {
+                nsecs=false;
+            }
+            break;
+        default:
+            printf("Invalid choice inputted.Exiting....\n");
+            exit(EXIT_FAILURE);
     }
-    
+    i=0;
+    while(i<2)
+    {
+        if(nsecs)
+        {
+            execute_bschedule();
+            if(nanosleep(&tim,NULL)==-1)
+            {
+                switch (errno) {
+                        case EINTR:
+                            printf("interrupted by a signal handler\n");
+                            exit(EXIT_FAILURE);
+                        case EINVAL:
+                            printf("tv_nsec - not in range or tv_sec is negative\n");
+                            exit(EXIT_FAILURE);
+                        default:
+                            perror("nanosleep");
+                            exit(EXIT_FAILURE);
+                    }
+            }
+        }
+        else{
+            execute_bschedule();
+            sleep(time_in_seconds);
+        }
+        i++;
+    }
     return 0;
+}
+
+void execute_bschedule()
+{
+    //calls the bscheduling file from here maybe
+    system("./out");
+    // _execl();
+    printf("Finished\n ");
 }
